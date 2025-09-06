fa9e4335c1d3fd6dddf29833d05e3a0751608ff2
SimpsMbatha <simbatha41@gmail.com>
2025-09-06 14:24:28 +0200

    added live time

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//search engine functionality
 function submitCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
@@ -6,3 +7,23 @@ function submitCity(event) {
 }
 let formButton = document.querySelector("#search-form");
 formButton.addEventListener("submit", submitCity);
+
+//time display
+let now = new Date();
+let currentTime = document.querySelector("#time");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+if (hours < 10) hours = `0${hours}`;
+if (minutes < 10) minutes = `0${minutes}`;
+currentTime.innerHTML = `${day}, ${hours}:${minutes}`;
